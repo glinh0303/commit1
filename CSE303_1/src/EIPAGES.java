a2414958673fb39034a5cd3a31c788bb22ef51e7
glinh0303 <109055868+glinh0303@users.noreply.github.com>
2024-07-10 15:32:39 +0700

    commit 1

---
+++ CSE303_1/src/EIPAGES.java
@@ -0,0 +1,139 @@
+import java.io.*;
+import java.util.*;;
+
+public class EIPAGES {
+
+	public static void main(String[] args) {
+		int n = ni();
+		int[] pages = new int[n];
+
+		for (int i = 0; i < pages.length; i++) {
+			int page = ni();
+			pages[i] = page;
+		}
+
+		Arrays.sort(pages);
+		StringBuilder sb = new StringBuilder();
+
+		int count = 0;
+		for (int i = 0; i < pages.length;) {
+			if (i < pages.length - 1) {
+				if (pages[i] == (pages[i + 1] - 1)) {
+					count++;
+					i++;
+					continue;
+				}
+			}
+			if (count > 1) {
+				sb.append(pages[i - count] + "-" + pages[i]);
+			} else if (count == 1) {
+				sb.append(pages[i - count] + " " + pages[i]);
+			} else {
+				sb.append(pages[i]);
+			}
+			sb.append(" ");
+			count = 0;
+			i++;
+		}
+		System.out.println(sb);
+	}
+
+	static InputStream is = System.in;
+	static byte[] inbuf = new byte[1 << 24];
+	static int lenbuf = 0, ptrbuf = 0;
+
+	static int readByte() {
+		if (lenbuf == -1)
+			throw new InputMismatchException();
+		if (ptrbuf >= lenbuf) {
+			ptrbuf = 0;
+			try {
+				lenbuf = is.read(inbuf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (lenbuf <= 0)
+				return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+
+	static boolean isSpaceChar(int c) {
+		return !(c >= 33 && c <= 126);
+	}
+
+	static int skip() {
+		int b;
+		while ((b = readByte()) != -1 && isSpaceChar(b))
+			;
+		return b;
+	}
+
+	static double nd() {
+		return Double.parseDouble(ns());
+	}
+
+	static char nc() {
+		return (char) skip();
+	}
+
+	static String ns() {
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while (!(isSpaceChar(b))) {
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+
+	static char[] ns(int n) {
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while (p < n && !(isSpaceChar(b))) {
+			buf[p++] = (char) b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+
+	static int ni() {
+		int num = 0, b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	static long nl() {
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+}
